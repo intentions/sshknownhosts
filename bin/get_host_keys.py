de2427b2b289fb7070d1da34a9d6b47889c2624d
Kurt Strosahl <strosahl@jlab.org>
2014-05-23 10:41:23 -0400

    generates a file containing the host key from the server 	on which it is run 	new file:   bin/get_host_keys.py 	reads through the data directory and builds a known_host file 	from the files found within 	renamed:    bin/host_key_harvester.py -> bin/known_hosts_builder.py

---
+++ bin/get_host_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+"""
+This program runs on build, getting the host key for the server on which it is running
+then placing that key in the data directory.
+"""
+
+import os, import sys, import subprocess
+
+
